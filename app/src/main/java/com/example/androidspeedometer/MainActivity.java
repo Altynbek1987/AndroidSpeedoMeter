c248c3929f06ef8f97b3438c65d108f0c010dccd
Altynbek1987 <hyundai8788@gmail.com>
2020-07-28 10:18:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidspeedometer/MainActivity.java
@@ -0,0 +1,205 @@
+package com.example.androidspeedometer;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationService myService;
+    static boolean status;
+    LocationManager locationManager;
+    static TextView dist, time, speed;
+    Button start, pause, stop;
+    static long startTime, endTime;
+    ImageView image;
+    static ProgressDialog locate;
+    static int p = 0;
+
+    private ServiceConnection sc = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            LocationService.LocalBinder binder = (LocationService.LocalBinder) service;
+            myService = binder.getService();
+            status = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            status = false;
+        }
+    };
+
+    void bindService() {
+        if (status == true)
+            return;
+        Intent i = new Intent(getApplicationContext(), LocationService.class);
+        bindService(i, sc, BIND_AUTO_CREATE);
+        status = true;
+        startTime = System.currentTimeMillis();
+    }
+
+    void unbindService() {
+        if (status == false)
+            return;
+        Intent i = new Intent(getApplicationContext(), LocationService.class);
+        unbindService(sc);
+        status = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (status == true)
+            unbindService();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (status == false)
+            super.onBackPressed();
+        else
+            moveTaskToBack(true);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dist = (TextView) findViewById(R.id.distancetext);
+        time = (TextView) findViewById(R.id.timetext);
+        speed = (TextView) findViewById(R.id.speedtext);
+
+        start = (Button) findViewById(R.id.start);
+        pause = (Button) findViewById(R.id.pause);
+        stop = (Button) findViewById(R.id.stop);
+
+        image = (ImageView) findViewById(R.id.image);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Приведенный ниже метод проверяет, включено ли GPS на устройстве или нет. Если нет, то появляется диалоговое окно с предупреждением
+                //включить GPS.
+                checkGps();
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                    return;
+                }
+                Toast.makeText(MainActivity.this, "GPS включен в вашем устройстве", Toast.LENGTH_SHORT).show();
+
+                if (status == false)
+                    //Здесь Служба определения местоположения становится связанной, а GPS-спидометр становится активным.
+                    bindService();
+                locate = new ProgressDialog(MainActivity.this);
+                locate.setIndeterminate(true);
+                locate.setCancelable(false);
+                locate.setMessage("\n" + "Получение местоположения...");
+                locate.show();
+                start.setVisibility(View.GONE);
+                pause.setVisibility(View.VISIBLE);
+                pause.setText("Pause");
+                stop.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pause.getText().toString().equalsIgnoreCase("pause")) {
+                    pause.setText("Resume");
+                    p = 1;
+
+                } else if (pause.getText().toString().equalsIgnoreCase("Resume")) {
+                    checkGps();
+                    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        return;
+                    }
+                    pause.setText("Pause");
+                    p = 0;
+
+                }
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (status == true)
+                    unbindService();
+                start.setVisibility(View.VISIBLE);
+                pause.setText("Pause");
+                pause.setVisibility(View.GONE);
+                stop.setVisibility(View.GONE);
+                p = 0;
+            }
+        });
+    }
+    //Этот метод приводит к диалоговому окну предупреждения.
+    void checkGps() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+
+            showGPSDisabledAlertToUser();
+        }
+    }
+
+    //Этот метод настраивает диалоговое окно Alert.
+    private void showGPSDisabledAlertToUser() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("\n" +
+                "Включить GPS для использования приложения")
+                .setCancelable(false)
+                .setPositiveButton("Включить GPS",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Отмена",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+}
